de52e0f9c1686a55bb76e7963c5812a0868ec8ef
Daniel Okwufulueze <daniel.okwufulueze@gmail.com>
2017-09-21 13:55:04 +0100

    ODLL-006: Add JSON collection of American states. Add form validations.

--- src/components/main/view-sections/UserForm.vue
+++ src/components/main/view-sections/UserForm.vue
@@ -3,33 +3,37 @@
     <div class="title">Register as Patient</div>
     <div class="field">
       <label for="name" class="field-key">Your Name</label>
-      <input type="text" class="field-value" id="name" v-model="user.fullName">
+      <input type="text" class="field-value has-tip" id="name" v-model="user.fullName" placeholder="Your Name" @input="displayLabel">
+      <div class="tip">Write between 5 to 32 characters</div>
     </div>
 
     <div class="field">
       <label for="email" class="field-key">Your Email</label>
-      <input type="text" class="field-value" id="email" v-model="user.email">
+      <input type="email" class="field-value has-tip" id="email" v-model="user.email" placeholder="Your Email" @input="displayLabel">
+      <div class="tip">Write between 5 to 32 characters</div>
     </div>
 
     <div class="field avatar"></div>
 
     <div class="field">
       <label for="state" class="field-key">State</label>
-      <input type="text" class="field-value" id="state">
+      <select id='state' class="field-value" @input="displayLabel"><option value=''>State</option></select>
     </div>
 
     <div class="field">
       <label for="residential-address" class="field-key">Residential Address</label>
-      <input type="text" class="field-value" id="residential-address" v-model="user.residentialAddress">
+      <input type="text" class="field-value has-tip" id="residential-address" v-model="user.residentialAddress" placeholder="Residential Address" @input="displayLabel">
+      <div class="tip">Write between 5 to 32 characters</div>
     </div>
 
     <div class="field">
       <label for="phone-number" class="field-key">Phone Number</label>
-      <input type="text" class="field-value" id="phone-number" v-model="user.phoneNumber">
+      <input type="text" class="field-value has-tip" id="phone-number" v-model="user.phoneNumber" placeholder="Phone Number" @input="displayLabel">
+      <div class="tip">Write between 5 to 32 characters</div>
     </div>
 
     <div class="field">
-      <label class="field-key">Birthday</label>
+      <label class="field-key date-of-birth">Birthday</label>
       <div class='date-holder'>
         <select id='day'><option value=''>Day</option></select>
         <select id='month'><option value=''>Month</option></select>
@@ -65,12 +69,45 @@
           avatarCanvas.style.borderRadius = '100px'
           avatarContainer.appendChild(avatarCanvas)
         }
+      },
+      populateStates () {
+        Object.keys(states).forEach((id) => {
+          const stateName = states[id]
+          const optionElement = document.createElement('option')
+          optionElement.text = stateName
+          optionElement.value = id
+          const statesElement = document.getElementById('state')
+          if (statesElement) {
+            statesElement.appendChild(optionElement)
+          }
+        })
+      },
+      displayLabel (evt) {
+        const target = evt.target
+        const id = target.id
+        document.querySelector(`label[for=${id}]`).style.display = target.value === '' ? 'none' : 'block'
+        if (target.classList.contains('has-tip')) this.warnOfInputLength(target)
+      },
+      warnOfInputLength (target) {
+        const tip = target.nextElementSibling
+        const entryValue = target.value.trim()
+
+        if (entryValue !== '' && entryValue.length < 5) {
+          tip.innerHTML = 'Type more than 5 characters'
+        } else if (entryValue !== '' && entryValue.length > 32) {
+          tip.innerHTML = 'You have exceeded 32 characters'
+        } else {
+          tip.innerHTML = 'Write between 5 to 32 characters'
+        }
+
+        tip && tip.classList && entryValue !== '' && (entryValue.length < 5 || entryValue.length > 32) ? tip.classList.add('error') : tip.classList.remove('error')
       }
     },
     mounted: function () {
       dateSelectionManager.loadDate()
       document.querySelector('#user-form').closest('.content').style.borderTop = 'none'
       this.addAvatar()
+      this.populateStates()
     },
     watch: {
       avatarCanvas () {
@@ -80,6 +117,7 @@
   }
 
   import dateSelectionManager from 'date-selection-manager'
+  import states from '../../../../static/json/states/states_hash.json'
 </script>
 
 <style scoped>
@@ -97,7 +135,8 @@
   }
 
   .title {
-    height: 40px;
+    height: 25px;
+    margin-bottom: 50px;
     width: 100%;
     font-size: 20px;
     color: #4d4c49;
@@ -109,14 +148,27 @@
     width: 100%;
   }
 
-  .field-key {
+  .field-key:not(.date-of-birth) {
     height: 20px;
-    display: block;
+    display: none;
   }
 
   .field-value {
     height: 30px;
     width: 100%;
+  }
+
+  .tip {
+    width: 100%;
+    color: #adadad;
+  }
+
+  .tip.error {
+    color: #f18787;
+  }
+
+  :placeholder {
+    color: #adadad;
   }
 
   .side-key {
@@ -131,7 +183,7 @@
     height: 130px;
   }
 
-  input[type=text].field-value, select {
+  input[type=email].field-value, input[type=text].field-value, select {
     outline: none;
     border-width: 0px 0px 2px 0px;
     border-color: #a0a0a0;
@@ -153,5 +205,9 @@
     height: 30px;
     min-width: 100px;
     margin-right: 20px;
+  }
+
+  input:not(:placeholder-shown):not(:focus):invalid {
+    border-bottom: 2px solid #f18787;
   }
 </style>
