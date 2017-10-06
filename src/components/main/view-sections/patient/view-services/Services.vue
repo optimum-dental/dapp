0916e0ba3931ee6c1c5b8d89128f4d7a2cf1acc5
Daniel Okwufulueze <daniel.okwufulueze@gmail.com>
2017-10-06 23:53:23 +0100

    ODLL-008: Setup Services.

--- src/components/main/view-sections/patient/view-services/Services.vue
+++ src/components/main/view-sections/patient/view-services/Services.vue
@@ -1,10 +1,53 @@
 <template>
   <div class="wrapper">
-    <div id="services"></div>
+    <div id="services">{{totalRows}}
+      <div class="title">Our Services</div>
+      <div class="row" v-for="i in totalRows">
+        <div class="column" v-for="j in 3">
+          <div class="item" v-for="k in 4">
+            <div class="item-bullet"></div>
+            <div class="item-text"></div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
-<script type="text/javascript"></script>
+<script type="text/javascript">
+  export default {
+    computed: {
+      totalRows () {
+        return Math.ceil(this.services.length / 12)
+      }
+    },
+    data: function () {
+      return {
+        services: [
+          '',
+          'Anti-Snoring Treatment',
+          'Bonding',
+          'Crown and Bridgework',
+          'Custom Mouth Guards',
+          'Dentures',
+          'Fillings',
+          'Fluoride Treatments',
+          'Implants',
+          'Oral Surgery',
+          'Orthodontics',
+          'Periodontics',
+          'Root Canal Treatments',
+          'Routine Care',
+          'Sealants',
+          'Sedation',
+          'Veneers',
+          'Whitening',
+          'X-Rays'
+        ]
+      }
+    }
+  }
+</script>
 
 <style scoped>
   .wrapper {
@@ -28,5 +71,52 @@
     display: flex;
     flex-direction: column;
     align-items: center;
+  }
+
+  .title {
+    height: 40px;
+    line-height: 40px;
+    width: 100%;
+    text-align: left;
+    font-size: 20px;
+    margin-bottom: 20px;
+  }
+
+  .row {
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .column {
+    /*width: 31%;*/
+    /*display: inline-block;*/
+    /*margin-right: 2%;*/
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .item {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    height: 40px;
+  }
+
+  .item-bullet {
+    height: 10px;
+    width: 10px;
+    background: #3384b2;
+  }
+
+  .item-text {
+    height: 10px;
+    width: 10px;
+    background: #3384b2;
   }
 </style>
